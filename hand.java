ddfb261f83773f6c3b93673de0b13042443afbb9
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-03-24 14:38:59 +0900

    Update hand.java

--- hand.java
+++ hand.java
@@ -13,6 +13,42 @@ public class Hand {
     }
   }
   int[] ranks = new int[14];
-  int[]orderedRanks = new int[5];
+  int[] orderedRanks = new int[5];
+  boolean flush=true, straight=false;
+  int sameCards=1,sameCards2=1;
+  int index=0;
+  int topStraightValue=0;
 
+  for (int x=0; x<=13; x++)
+  {
+    ranks[x]=0;
+  }
+  for (int x=0; x<=4; x++)
+  {
+    ranks[ cards[x].getRank() ]++;
+  }
+  for (int x=0; x<4; x++)
+  {
+    if ( cards[x].getSuit() != cards[x+1].getSuit() )
+        flush=false;
+  }
+
+  for (int x=13; x>=1; x--)
+  {
+    if (ranks[x] > sameCards)
+    {
+      if (sameCards1 !=1 )
+     {
+      sameCards2 = sameCards;
+      smallGroupRank = largeGroupRank;
+     } 
+     sameCards = ranks[x];
+     largeGroupRank = x;
+  } else if (ranks[x] > sameCards2)
+  {
+    sameCards2 = ranks[x];
+    smallGroupRank = x;
+  }
 }
+
+
