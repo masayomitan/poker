83ec0f8a7380e49850862574d1ec3630b28f3d5a
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-03-24 14:15:03 +0900

    hand.java

--- hand.java
+++ hand.java
@@ -1,5 +1,18 @@
 package javapoker;
-public class Hand (
+public class Hand {
   private Card[] cards;
   private int[] value;
-)
+
+  Hand(Deck d)
+  {
+    value = new int[6];
+    cards = new Card[5];
+    for (int i =0; x<5; x++)
+    {
+      cards[x] =d.drawFromDeck();
+    }
+  }
+  int[] ranks = new int[14];
+  int[]orderedRanks = new int[5];
+
+}
